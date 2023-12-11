fcf795503e4a6cd135e51178618d088a7a0cfb57
Hornet9 <ang.peng.xuan@gmail.com>
2023-12-11 21:23:43 +0800

    working thruster code

--- thrusters.py
+++ thrusters.py
@@ -3,22 +3,22 @@ import time
 bus = can.interface.Bus(interface="socketcan", channel="can0", bitrate=500000)
 
 #use decimal values, last 2 are useless values
-thrusters = [70, 70, 70, 70, 70, 70, 0, 0]
-
-t_msg = can.Message(
-    arbitration_id=0,
-    data = thrusters,
-    is_extended_id = False
-)
-
+x = 127
 #test
-
+# integrate with control the x need to chnage and the if else remove
 with bus as bus:
     while True:
         try:
+            t_msg = can.Message(
+                arbitration_id = 0, data = [x, x, x, x, x, x, 0, 0], is_extended_id = False
+            )
             bus.send(t_msg)
-            print("message sent")
-            time.sleep(1)
+            print("thruster_val", x)
+            time.sleep(0.5)
+            if x == 170:
+                x = 127
+            else:
+                x = 140
         except can.CanError as error:
             print(error)
             print("message not sent")
