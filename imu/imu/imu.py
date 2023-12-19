7a1e10b6381d3f53d0672d35eb08c198e36b9c3c
bb-jetson-2023 <bb.jetson.2023@gmail.com>
2023-12-19 15:28:59 +0800

    Fixed IMU publishing

--- imu/imu/imu.py
+++ imu/imu/imu.py
@@ -1,78 +1,77 @@
 import can
 import rclpy
-from rclpy.node import Node
-
-from std_msgs.msg import Header
-from linear_acceleration_msg.msg import AccelerationXY, AccelerationZ
-from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion, Vector3
+from linear_acceleration_msg.msg import AccelerationXY, AccelerationZ
+from rclpy.node import Node
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Header
 
 IMU_DICT = {
-    'a_x': None,
-    'a_y': None,
-    'a_z': None,
-    'q_x': None,
-    'q_y': None,
-    'q_z': None,
-    'q_w': None
+    "a_x": None,
+    "a_y": None,
+    "a_z": None,
+    "q_x": None,
+    "q_y": None,
+    "q_z": None,
+    "q_w": None,
 }
+
 
 class MinimalPublisher(Node):
     def __init__(self):
-        super().__init__('imu')
-        self.publisher_ = self.create_publisher(Imu, '/sensors/imu', 10)
+        super().__init__("imu")
+        self.publisher_ = self.create_publisher(Imu, "/sensors/imu", 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.bus = can.interface.Bus(interface="socketcan", channel="can0", bitrate=500000)
+        self.bus = can.interface.Bus(
+            interface="socketcan", channel="can0", bitrate=500000
+        )
 
     def timer_callback(self):
         global IMU_DICT
         with self.bus as bus:
             for msg in bus:
-                #filter for canbus id
+                # filter for canbus id
                 msg_id = msg.arbitration_id
 
-                #Quaternion
-                if (msg_id == 1):
+                # Quaternion
+                if msg_id == 1:
                     print("message id: ", msg_id)
-                    #print(msg.data)
-                    temp = list(msg.data) #msg.data is byte array, entries are in hex.
-                    msg_data = []
-                    for i in range(0 ,len(temp), 2):
-                        #left shift, or operations
-                        msg_data.append(float((temp[i] << 8 | temp[i + 1])))
-            
-                    print(msg_data, type(msg_data))
-                    IMU_DICT['q_x'] = msg_data[0]
-                    IMU_DICT['q_y'] = msg_data[1]
-                    IMU_DICT['q_z'] = msg_data[2]
-                    IMU_DICT['q_w'] = msg_data[3]
-                
-                #Acceleration xy
-                elif (msg_id == 2):
-                    print("message id: ", msg_id)
-                    #print(msg.data)
-                    temp = list(msg.data) #msg.data is byte array, entries are in hex.
-                    msg_data = []
-                    for i in range(0 ,len(temp), 4):
-                        #left shift, or operations
-                        # check the shift amount the excel say 4 used
-                        msg_data.append(float(temp[i] << 24 | temp[i+1] << 16 | temp[i+2] << 8 | temp[i+3]))
-            
-                    print(msg_data, type(msg_data))
-                    IMU_DICT['a_x'] = msg_data[0]
-                    IMU_DICT['a_y'] = msg_data[1]
 
-                #Acceleration z
-                elif (msg_id == 3):
+                    IMU_DICT["q_x"] = float(
+                        int.from_bytes(msg.data[:2], "big", signed=True)
+                    )
+                    IMU_DICT["q_y"] = float(
+                        int.from_bytes(msg.data[2:4], "big", signed=True)
+                    )
+                    IMU_DICT["q_z"] = float(
+                        int.from_bytes(msg.data[4:6], "big", signed=True)
+                    )
+                    IMU_DICT["q_w"] = float(
+                        int.from_bytes(msg.data[6:], "big", signed=True)
+                    )
+
+                # Acceleration xy
+                elif msg_id == 2:
                     print("message id: ", msg_id)
-                    #print(msg.data)
-                    temp = list(msg.data) #msg.data is byte array, entries are in hex.
-                    
-                    IMU_DICT['a_z'] = float(temp[0] << 24 | temp[1] << 16 | temp[2] << 8 | temp[3])
-                
-                #if any None values, continue reading from canbus
+                    IMU_DICT["a_x"] = float(
+                        int.from_bytes(msg.data[:4], "big", signed=True)
+                    )
+                    IMU_DICT["a_y"] = float(
+                        int.from_bytes(msg.data[4:], "big", signed=True)
+                    )
+                    print(IMU_DICT["a_x"], IMU_DICT["a_y"])
+
+                # Acceleration z
+                elif msg_id == 3:
+                    print("message id: ", msg_id)
+                    IMU_DICT["a_z"] = float(
+                        int.from_bytes(msg.data[:4], "big", signed=True)
+                    )
+                    print(IMU_DICT["a_z"])
+
+                # if any None values, continue reading from canbus
                 if None in IMU_DICT.values():
                     continue
 
@@ -81,35 +80,27 @@ class MinimalPublisher(Node):
                 imu_msg.header = Header()
                 imu_msg.header.frame_id = str(msg.arbitration_id)
 
-                #acceleration
+                # acceleration
                 imu_msg.linear_acceleration = Vector3()
-                imu_msg.linear_acceleration.x = IMU_DICT['a_x']
-                imu_msg.linear_acceleration.y = IMU_DICT['a_y']
-                imu_msg.linear_acceleration.z = IMU_DICT['a_z']
+                imu_msg.linear_acceleration.y = IMU_DICT["a_y"]
+                imu_msg.linear_acceleration.x = IMU_DICT["a_x"]
+                imu_msg.linear_acceleration.z = IMU_DICT["a_z"]
 
-                #Quaternion
+                # Quaternion
                 # Fill in the orientation (quaternion)
-                #imu_msg.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)  # Adjust the values accordingly
+                # imu_msg.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)  # Adjust the values accordingly
                 imu_msg.orientation = Quaternion()  # Adjust the values accordingly
-                imu_msg.orientation.x = IMU_DICT['q_x']
-                imu_msg.orientation.y = IMU_DICT['q_y']
-                imu_msg.orientation.z = IMU_DICT['q_z']
-                imu_msg.orientation.w = IMU_DICT['q_w']
+                imu_msg.orientation.x = IMU_DICT["q_x"]
+                imu_msg.orientation.y = IMU_DICT["q_y"]
+                imu_msg.orientation.z = IMU_DICT["q_z"]
+                imu_msg.orientation.w = IMU_DICT["q_w"]
 
                 self.publisher_.publish(imu_msg)
-                self.get_logger().info(f"linear acceleration published: {imu_msg.linear_acceleration}")
+                self.get_logger().info(
+                    f"linear acceleration published: {imu_msg.linear_acceleration}"
+                )
                 self.get_logger().info(f"orientation published: {imu_msg.orientation}")
 
-                #reset imu values
-                # IMU_DICT = {
-                #     'a_x': None,
-                #     'a_y': None,
-                #     'a_z': None,
-                #     'q_x': None,
-                #     'q_y': None,
-                #     'q_z': None,
-                #     'q_w': None
-                # }
 
 def main(args=None):
     rclpy.init(args=args)
@@ -125,7 +116,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
