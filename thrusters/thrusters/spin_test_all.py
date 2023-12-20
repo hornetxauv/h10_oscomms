dd9f6382db933453dd80066921b4910092ddff6a
SamuelFoo <fooenzesamuel@gmail.com>
2023-12-20 20:28:38 +0800

    thruster biases

--- thrusters/thrusters/spin_test_all.py
+++ thrusters/thrusters/spin_test_all.py
@@ -3,17 +3,29 @@ import time
 from thrusters import ThrusterControl
 
 thrusterControl = ThrusterControl()
-thrustMag = 170
-duration = 5
 
+print("Start")
+time.sleep(30)
 # Spin all forward.
-thrusterControl.setThrusters([thrustMag] * 6)
+for i in range(5):
+    thrusterControl.killThrusters()
+    time.sleep(1)
+
+thrustMag = 170
+ZERO = 127
+duration = 30
+# thrusterValues = [170, 172, ZERO, ZERO, ZERO, ZERO]
+# thrusterValues = [ZERO, ZERO, ZERO, ZERO, 172, 170]
+# thrusterValues = [ZERO, ZERO, 153, 150, ZERO, ZERO]
+thrusterValues = [170, 172, 101, 104, 172, 170]
+# Spin all forward.
+thrusterControl.setThrusters(thrusterValues)
 time.sleep(duration)
 thrusterControl.killThrusters()
 time.sleep(0.5)
 
-# Spin all backward.
-thrusterControl.setThrusters([255 - thrustMag] * 6)
-time.sleep(duration)
-thrusterControl.killThrusters()
-time.sleep(0.5)
+# # Spin all backward.
+# thrusterControl.setThrusters([255 - thrustMag] * 6)
+# time.sleep(duration)
+# thrusterControl.killThrusters()
+# time.sleep(0.5)
