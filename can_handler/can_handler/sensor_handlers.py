4bbf5413a88adb219d4272f2e5aba30c3069f36d
hxbb2024 <hxbb2024@gmail.com>
2025-02-19 17:19:47 +0800

    removed rolling median shit

--- can_handler/can_handler/sensor_handlers.py
+++ can_handler/can_handler/sensor_handlers.py
@@ -80,7 +80,7 @@ class DepthIMUHandler(SensorHandler):
         self.last_yaw = 0
 
         # rolling median
-        self.yaw_buffer = deque(maxlen=5)
+        # self.yaw_buffer = deque(maxlen=5)
 
     # def process_data(self, data: Union[int, tuple[int, int]], msg_id=0):
     def process_data(self, data, msg_id=0):
@@ -108,14 +108,14 @@ class DepthIMUHandler(SensorHandler):
             elif (real_yaw_diff > 180): # wrap around case turning left. values fgo from negative to positive
                 real_yaw_diff -= 360
             self.last_yaw = new_yaw
-            # Add the new yaw difference to the rolling buffer
-            self.yaw_buffer.append(self.depth_imu_msg.yaw + real_yaw_diff)
-            # Compute the rolling median
-            rolling_median_yaw = statistics.median(self.yaw_buffer)
+            # # Add the new yaw difference to the rolling buffer
+            # self.yaw_buffer.append(self.depth_imu_msg.yaw + real_yaw_diff)
+            # # Compute the rolling median
+            # rolling_median_yaw = statistics.median(self.yaw_buffer)
             print(f"Last yaw:{self.last_yaw}, New yaw: {new_yaw}, Real yaw diff: {real_yaw_diff}")
             new_depth = -max(min((new_depth+0.45), 2.0), 0.0)
             if (real_yaw_diff != 0):
-                self.depth_imu_msg.yaw = rolling_median_yaw
+                self.depth_imu_msg.yaw = new_yaw + real_yaw_diff
                 self.new_dy = True
                 # self._update_timer["yaw"] = time.time()
             if (self.depth_imu_msg.depth != new_depth):
